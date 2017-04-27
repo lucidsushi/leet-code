c381421f4487bfba721270d219cdb425ef2f8296
Sushi Girl <snowbow2@gmail.com>
2017-04-26 20:09:34 -0400

    first attempt

--- algo2_addtwonumbers.py
+++ algo2_addtwonumbers.py
@@ -23,3 +23,28 @@ def addTwoNumbers(self, l1, l2):
     :type l2: ListNode
     :rtype: ListNode
     """
+    l_sum = None
+    l_ret = None
+    base = 0
+    while True:
+        l1_val = 0
+        l2_val = 0
+        if l1:
+            l1_val = l1.val
+            l1 = l1.next
+        if l2:
+            l2_val = l2.val
+            l2 = l2.next
+        digit = (l1_val + l2_val + base) % 10
+        base = (l1_val + l2_val + base)//10
+        if not l_sum:
+            l_sum = ListNode(digit)
+            l_ret = l_sum
+        else:
+            l_sum.next = ListNode(digit)
+            l_sum = l_sum.next
+        if not l1 and not l2 and base:
+            l_sum.next = ListNode(base)
+        if not l1 and not l2:
+            break
+    return l_ret
