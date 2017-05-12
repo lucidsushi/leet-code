c0e12e93a478ec17395f41993a03df06a9329a68
Sushi <lucidsushi@users.noreply.github.com>
2017-05-12 09:55:51 -0400

    looked at editorial solution to see how to do it recursively, and also better O(1) space iterative approach

--- 206_algo_reverseLinkedlist.py
+++ 206_algo_reverseLinkedlist.py
@@ -1,24 +1,32 @@
 #https://leetcode.com/problems/reverse-linked-list/#/description
 
-# Reverse a singly linked list.
-
 # Definition for singly-linked list.
 # class ListNode(object):
 #     def __init__(self, x):
 #         self.val = x
 #         self.next = None
 
+def reverseList_recursion(head):
+    if not head or not head.next:
+        return head
+    p = reverseList_(head.next)
+    head.next.next = head
+    head.next = None
+    return p
+
+def reverseList_iteration(head):
+    prev = None
+    while head:
+        next_head, head.next = head.next, prev
+        prev = head
+        head = next_head
+    return prev
+
 class Solution(object):
+    """
+    :type head: ListNode
+    :rtype: ListNode
+    """
     def reverseList(self, head):
-        """
-        :type head: ListNode
-        :rtype: ListNode
-        """
-        heads = []
-        reverse_root = reverse = ListNode(0)
-        while head:
-            heads.append(head.val)
-            head = head.next
-        while heads:
-            reverse.next = reverse = ListNode(heads.pop())
-        return reverse_root.next
+        return reverseList_recursion(head)
+        #return reverseList_iteration(head)
