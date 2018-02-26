d637d580690e7f339d963186dc28e9e5e951b81e
Sushi Girl <snowbow2@gmail.com>
2018-02-25 22:28:39 -0500

    reverse linked list updates?

--- 206_algo_reverseLinkedlist.py
+++ 206_algo_reverseLinkedlist.py
@@ -3,10 +3,10 @@
 # Reverse a singly linked list.
 
 # Definition for singly-linked list.
-# class ListNode(object):
-#     def __init__(self, x):
-#         self.val = x
-#         self.next = None
+class ListNode(object):
+    def __init__(self, x):
+        self.val = x
+        self.next = None
 
 class Solution(object):
     def reverseList(self, head):
@@ -22,3 +22,43 @@ class Solution(object):
         while heads:
             reverse.next = reverse = ListNode(heads.pop())
         return reverse_root.next
+
+
+
+
+reverse(A B C D E F):
+head = A
+second = head.next = B
+reversed_rest = reverse(second) = F E D C B
+what do we do with B now?
+what do we do with A now?
+
+
+in the recursive strategy, you save a reference to B as second
+and call reverse from B onwards
+which gives you back a reference to F
+so since you still have a reference to B, you can then do B.next = A
+
+
+head = A
+head_next = head.next = B
+
+head_dummy = head
+second_ = head.next
+return reverse(head_dummy, head, second_)
+def reverse(head):
+    second = head.next
+    if not second:
+        second_.next = head_dummy
+    second.next = head
+    return reverse(second)
+    
+
+
+
+prev = None
+while head:
+    next_head, head.next = head.next, prev
+    prev = head
+    head = next_head
+return prev
