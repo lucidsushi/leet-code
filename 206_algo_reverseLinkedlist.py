d9a2ec6623936e5d43665455e7db2c3348232859
Sushi Girl <snowbow2@gmail.com>
2018-02-25 22:36:07 -0500

    commented out stuff im not sure what it does atm

--- 206_algo_reverseLinkedlist.py
+++ 206_algo_reverseLinkedlist.py
@@ -32,51 +32,53 @@ class Solution(object):
         :type head: ListNode
         :rtype: ListNode
         """
-        heads = []
-        reverse_root = reverse = ListNode(0)
-        while head:
-            heads.append(head.val)
-            head = head.next
-        while heads:
-            reverse.next = reverse = ListNode(heads.pop())
-        return reverse_root.next
+        return reverseList_recursion(head)
+        #return reverseList_iteration(head)
 
 
 
 
-reverse(A B C D E F):
-head = A
-second = head.next = B
-reversed_rest = reverse(second) = F E D C B
-what do we do with B now?
-what do we do with A now?
 
+# reverse(A B C D E F):
+# head = A
+# second = head.next = B
+# reversed_rest = reverse(second) = F E D C B
+# what do we do with B now?
+# what do we do with A now?
 
+'''
 in the recursive strategy, you save a reference to B as second
 and call reverse from B onwards
 which gives you back a reference to F
 so since you still have a reference to B, you can then do B.next = A
+'''
 
+# head = A
+# head_next = head.next = B
 
-head = A
-head_next = head.next = B
-
-head_dummy = head
-second_ = head.next
-return reverse(head_dummy, head, second_)
-def reverse(head):
-    second = head.next
-    if not second:
-        second_.next = head_dummy
-    second.next = head
-    return reverse(second)
+# head_dummy = head
+# second_ = head.next
+# return reverse(head_dummy, head, second_)
+# def reverse(head):
+#     second = head.next
+#     if not second:
+#         second_.next = head_dummy
+#     second.next = head
+#     return reverse(second)
     
-prev = None
-while head:
-    next_head, head.next = head.next, prev
-    prev = head
-    head = next_head
-return prev
+# prev = None
+# while head:
+#     next_head, head.next = head.next, prev
+#     prev = head
+#     head = next_head
+# return prev
 
-        return reverseList_recursion(head)
-        #return reverseList_iteration(head)
+
+# heads = []
+# reverse_root = reverse = ListNode(0)
+# while head:
+#     heads.append(head.val)
+#     head = head.next
+# while heads:
+#     reverse.next = reverse = ListNode(heads.pop())
+# return reverse_root.next
