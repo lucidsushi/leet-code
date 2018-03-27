7260f5875f525e74ae91831a40b4d308e10386f4
dsu <dsu@spinvfx.com>
2018-03-27 18:41:28 -0400

    finally thought of the fastest method i know again..

--- 001_algo_twosum.py
+++ 001_algo_twosum.py
@@ -10,6 +10,8 @@
 # Because nums[0] + nums[1] = 2 + 7 = 9,
 # return [0, 1].
 
+
+# mchen style
 def twoSum(nums, target):
     """
     :type nums: List[int]
@@ -23,35 +25,8 @@ def twoSum(nums, target):
         except:
             _map[n] = i
 
-# print twoSum([2, 7, 11, 15], 9)
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-# refresher
+# slowest method thought of
 def addtwo(nums, target):
     i_to_try = 0
     while i_to_try < len(nums):
@@ -60,7 +35,27 @@ def addtwo(nums, target):
                 return i_to_try, i
         i_to_try += 1
 
-array = [2, 3, 4]  # [3, 3]
-target = 6
 
-print addtwo(array, target)
+# first realization of O(n)
+def addtwo(nums, target):
+    nums_dict = {}
+    for index, num in enumerate(nums):
+        if (target-num) in nums_dict and nums_dict[target-num] != index:
+            return (index, nums_dict[target-num])
+        else:
+            nums_dict[num] = index
+
+
+# rejection until match exist with less conditional checking
+def addtwo(nums, target):
+    nums_dict = {}
+    for index, num in enumerate(nums):
+        try:
+            return index, nums_dict[target-num]
+        except KeyError:
+            nums_dict[num] = index
+
+
+# print twoSum([2, 3, 4], 6)
+# print twoSum([3, 3], 6)
+# print twoSum([2, 7, 11, 15], 9)
